1f58231a77768ff71b7b5bc86ccdaaa37f74e6cb
A .M <ahmedzzabdalla0@gmail.com>
2025-11-17 13:22:29 +0200

    refactor(accordion): make it more simpler and moduler

--- app/components/shared/molecules/accordion.tsx
+++ app/components/shared/molecules/accordion.tsx
@@ -1,4 +1,4 @@
-// molecules/Accordion.tsx (NO "use client" needed!)
+// molecules.tsx (NO "use client" needed!)
 import { useId } from "react";
 import { IoIosArrowDropright } from "react-icons/io";
 import { twMerge } from "tailwind-merge";
@@ -19,32 +19,38 @@ export default function Accordion({
   const inputId = useId();
 
   return (
-    <div className="rounded-7 border border-secondary-100 p-4 mobile:p-6">
+    <div>
       <input
         type="radio"
         name={group}
         id={inputId}
-        className="peer/accordion hidden"
+        className="peer hidden"
         defaultChecked={defaultOpen}
       />
 
-      <label htmlFor={inputId} className="flex cursor-pointer justify-between">
-        <p className="text-neutral font-b-13">{title}</p>
-        <IoIosArrowDropright
-          className={twMerge(
-            "text-2xl text-secondary-200 transition-transform peer-checked/accordion:rotate-90 tablet:text-[36px]",
-          )}
-        />
-      </label>
+      <label
+        htmlFor={inputId}
+        className="group block rounded-7 border border-secondary-100 p-4 peer-not-checked:clickable mobile:p-6"
+      >
+        {/* Head */}
+        <div className="flex items-center justify-between">
+          <p className="text-neutral font-b-13">{title}</p>
+          <IoIosArrowDropright
+            className={twMerge(
+              "text-2xl text-secondary-200 transition-transform group-peer-checked:rotate-90 tablet:text-[36px]",
+            )}
+          />
+        </div>
 
-      {/* Tail (Content) */}
-      <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-300 peer-checked/accordion:grid-rows-[1fr]">
-        <div className="overflow-hidden">
-          <div className="mt-4 border-t border-dashed border-neutral pt-4 mobile:mt-6 mobile:pt-6">
-            <p className="text-neutral font-b-16">{description}</p>
+        {/* Tail (Content) */}
+        <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-300 group-peer-checked:grid-rows-[1fr]">
+          <div className="overflow-hidden">
+            <div className="mt-4 border-t border-dashed border-neutral pt-4 mobile:mt-6 mobile:pt-6">
+              <p className="text-neutral font-b-16">{description}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </label>
     </div>
   );
 }
